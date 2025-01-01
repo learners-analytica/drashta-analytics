dc81e4716507662f4f32d558a295b52627d544ca
Shaunak Perni <134259584+ShaunakPerni@users.noreply.github.com>
2025-01-01 18:23:22 +0530

    add CORS and init

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from service.types.appTypes import MLModel
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = FastAPI()
 
+# Initialize Quick Data
 
+ML_model = list[MLModel] = []
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = [os.getenv("ALLOW_ORIGINS", "http://localhost:3000")],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
