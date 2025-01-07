9bef8bfd361c79fd9cc134b1aa3a187d6d97f26e
Shaunak Perni <134259584+ShaunakPerni@users.noreply.github.com>
2025-01-07 19:47:59 +0530

    add new request

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastapi.encoders import jsonable_encoder
 from dotenv import load_dotenv
 import os
 from service.test import machine_test
+from service.auto_machine_learning import request_train_model_on_data
 
 load_dotenv()
 
@@ -26,3 +27,26 @@ async def machine_test_api():
     return jsonable_encoder(data)
 
 
+@app.post("/test/")
+async def machine_test_api(param1: str, param2: int):
+    # Use the parameters for processing
+    data = machine_test()
+    return jsonable_encoder(data)
+
+
+
+
+@app.post("/train/")
+async def train_model_api(data: dict, target_column: str, task_type: str, optimization_metric: str, time_limit: int, model_name: str, train_split: float, val_split: float, test_split: float):
+    model = request_train_model_on_data(
+        data=data,
+        target_column=target_column,
+        task_type=task_type,
+        optimization_metric=optimization_metric,
+        time_limit=time_limit,
+        model_name=model_name,
+        train_split=train_split,
+        val_split=val_split,
+        test_split=test_split
+    )
+    return jsonable_encoder(model)
