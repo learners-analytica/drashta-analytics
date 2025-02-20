577e369ac98a9ed75dc20cb612082cce106f8a22
Shaunak Perni <134259584+ShaunakPerni@users.noreply.github.com>
2025-02-20 22:22:43 +0530

    add data retrieval service

---
+++ services/bridge/data_retrieval.py
@@ -0,0 +1,31 @@
+import httpx
+import os
+import dotenv
+from drashta_types.drashta_types_table import TTableData
+from drashta_types.drashta_types_data import TDataArray
+import pandas
+dotenv.load_dotenv()
+
+async def request_table_data(table: str, columns: list[str], size: int) -> TTableData:
+    bridge_server_url = os.getenv("BRIDGE_SERVER")
+    
+    if not bridge_server_url:
+        raise RuntimeError("BRIDGE_SERVER environment variable is not set")
+    
+    async with httpx.AsyncClient() as client:
+        response:TTableData = await client.post(
+            f"{bridge_server_url}/supabase/get-table-data-raw",
+            json={"table": table, "columns": columns, "size": size}
+        )
+        
+        response.raise_for_status()
+        return response.json()  # Assuming response JSON can be mapped to TTableData
+    
+async def get_table_data(table:str,columns:list[str],size:int)->TDataArray:
+    return (await request_table_data(table,columns,size))["table_data_series"]
+
+async def get_table_dataframe(table:str,columns:list[str],size:int)->pandas.DataFrame:
+    
+    table_data:TDataArray = await get_table_data(table,columns,size)
+    df = pandas.DataFrame(table_data)
+    return df
