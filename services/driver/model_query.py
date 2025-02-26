6b9ae265c028be40ba19534f2b957283b3ddbbd2
ShaunakPerni <134259584+ShaunakPerni@users.noreply.github.com>
2025-02-26 16:46:35 +0530

    init driver

---
+++ services/driver/model_query.py
@@ -0,0 +1,36 @@
+from turtle import mode
+from ..automl.model_generation import generate_model
+from ..io.model_save import save_model_tensor, model_meta_data
+from ..bridge.data_retrieval import get_table_dataframe
+from drashta_types.drashta_types_key import MLTaskTypes
+from flaml.automl import AutoML
+from pydantic import BaseModel
+
+class TMLModelQuery(BaseModel):
+    table: str
+    x: list[str]
+    y: str
+    model_name: str
+    size: int = 1000
+    task: MLTaskTypes = MLTaskTypes.CLASSIFICATION
+
+def MLModelQueryHandle(
+    table:str,
+    x:list[str],
+    y:str,
+    model_name:str,
+    size:int = 1000,
+    task:MLTaskTypes = MLTaskTypes.CLASSIFICATION,
+    ):
+    cols = x + [y]
+    data = get_table_dataframe(table,cols,size)
+    model:AutoML = generate_model(data,x,y,task)
+    meta_data = model_meta_data(
+        model_name,
+        x,
+        y,
+        task,
+        model.best_estimator
+    )
+    model = save_model_tensor(meta_data,model)
+    return model
