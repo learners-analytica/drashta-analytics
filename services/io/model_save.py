01f0ef4fb00fd1bd0304cc3c5a4ad5ec4eb2b52c
ShaunakPerni <134259584+ShaunakPerni@users.noreply.github.com>
2025-02-26 16:29:21 +0530

    update io

--- services/io/model_save.py
+++ services/io/model_save.py
@@ -1,19 +1,35 @@
 import safetensors
+import uuid
+from drashta_types.drashta_types_model import TModelMetadata
+from drashta_types.drashta_types_key import MLTaskTypes
 from flaml import AutoML
 import datetime
+import dotenv
+import os
 
-def model_meta_data(model_name:str,x_var:list[str],target:list[str])->dict:
-    return {
-        "model_name":model_name,
-        "x_variables": x_var,
-        "target" : target,
-        "created" : datetime.datetime.now().isoformat()
-    }
+dotenv.load_dotenv()
 
-def save_model_tensor(model_name,x_var,target,model:AutoML,file_name:str):
-    metadata:dict = model_meta_data(model_name,x_var,target)
+def model_meta_data(model_name:str,x_var:list[str],target:list[str],task:MLTaskTypes,estimator:str)->TModelMetadata:
+    meta_data = TModelMetadata(
+        id=uuid.uuid4(),
+        model_name = model_name,
+        data = x_var,
+        target = target,
+        date = datetime.datetime.now().strftime("%Y-%m-%d"),
+        task = task,
+        estimator = estimator,
+    )
+    return meta_data
+
+def save_model_tensor(model_meta_data:TModelMetadata,model:AutoML):
+    model_dir = os.environ("MODEL_DIR")
+    file_name = f"{model_dir}/{model_meta_data.model_name}_{model_meta_data.id}.safetensors"
     safetensors.serialize_file(
         model,
         file_name,
-        metadata
+        model_meta_data
     )
+    return file_name
+
+def load_model_tensor(file_name):
+    return safetensors.deserialize(file_name)
