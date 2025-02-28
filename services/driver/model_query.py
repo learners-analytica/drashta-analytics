366799004cd17325d40bd43b3e633d5d05748793
ShaunakPerni <134259584+ShaunakPerni@users.noreply.github.com>
2025-02-28 12:28:57 +0530

    update model_query

--- services/driver/model_query.py
+++ services/driver/model_query.py
@@ -1,7 +1,10 @@
-from services.automl.model_generation import generate_model
-from services.io.model_save import save_model_tensor, model_meta_data
+import pandas
+from services.automl.model_generation import generate_model, predict_model
+from services.io.model_io import save_model_tensor, model_meta_data, load_model_tensor
 from services.bridge.data_retrieval import get_table_dataframe
+from services.io.model_database import fetch_model_data, Model_DB_Fields, add_new_model
 from drashta_types.drashta_types_key import MLTaskTypes
+from drashta_types.drashta_types_data import TDataArray
 from flaml.automl import AutoML
 from pydantic import BaseModel
 
@@ -15,21 +18,34 @@ class TMLModelQuery(BaseModel):
 
 def MLModelQueryHandle(
     table:str,
-    x:list[str],
+    x_columns:list[str],
     y:str,
     model_name:str,
     size:int = 1000,
     task:MLTaskTypes = MLTaskTypes.CLASSIFICATION,
     ):
-    cols = x + [y]
+    cols = x_columns + [y]
     data = get_table_dataframe(table,cols,size)
-    model:AutoML = generate_model(data,x,y,task)
+    model:AutoML = generate_model(data,x_columns,y,task)
     meta_data = model_meta_data(
         model_name,
-        x,
+        x_columns,
         y,
         task,
         model.best_estimator
     )
-    model = save_model_tensor(meta_data,model)
+    model_filename = save_model_tensor(meta_data,model)
+    add_new_model(meta_data,model_filename)
     return model
+
+def MLPredictHandle(
+    x:TDataArray,
+    model_id:str
+    ):
+    model_data:Model_DB_Fields = fetch_model_data(model_id)
+    model_tensor = load_model_tensor(model_data.file_path)
+    data = pandas.DataFrame(x)
+    preds = predict_model(model_tensor,data)
+    return preds
+    
+    
