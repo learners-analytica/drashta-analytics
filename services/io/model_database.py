4cbc536f501107a4a8146b23cbcd3149dfdaeba2
ShaunakPerni <134259584+ShaunakPerni@users.noreply.github.com>
2025-02-28 12:28:45 +0530

    add read and write statements to model_database

--- services/io/model_database.py
+++ services/io/model_database.py
@@ -1,5 +1,4 @@
 from typing import Annotated
-
 from fastapi import Depends, FastAPI, HTTPException, Query
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 from drashta_types.drashta_types_model import TModelMetadata
@@ -8,7 +7,6 @@ import datetime
 from pydantic import create_model
 import dotenv
 import os
-import json
 
 dotenv.load_dotenv()
 
@@ -20,6 +18,7 @@ class Model_DB_Fields(SQLModel, table=True):
     date: datetime.datetime = Field(default_factory=datetime.datetime.utcnow)
     task: MLTaskTypes
     estimator: str
+    file_path:str
 
 db_file = os.environ("MODEL_DB_FILE")
 db_url = f"sqlite:///{db_file}"
@@ -27,5 +26,26 @@ db_url = f"sqlite:///{db_file}"
 connect_args = {"check_same_thread": False}
 engine = create_engine(db_url, connect_args=connect_args)
 
-def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)
+SQLModel.metadata.create_all(engine)
+    
+def fetch_model_data(id:str)->Model_DB_Fields:
+    with Session(engine) as session:
+        statement = select(Model_DB_Fields).where(Model_DB_Fields.id == id)
+        model_data = session.exec(statement).first()
+        return model_data
+
+def add_new_model(model_meta:TModelMetadata,file_name:str):
+    model_entry = Model_DB_Fields(
+        id = model_meta.id,
+        model_name= ",".join(model_meta.data),
+        data = model_meta.data,
+        target= model_meta.target,
+        task=model_meta.task,
+        estimator=model_meta.estimator,
+        file_path = file_name
+    )
+    with Session(engine) as session:
+        session.add(model_entry)
+        session.commit()
+    
+    
