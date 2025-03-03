4dcdd7854107c4156390d1a0203054850565effc
Shaunak Perni <134259584+ShaunakPerni@users.noreply.github.com>
2025-03-03 15:09:55 +0530

    add retrieve table meta data

--- services/bridge/data_retrieval.py
+++ services/bridge/data_retrieval.py
@@ -1,8 +1,8 @@
 import httpx
 import os
 import dotenv
-from drashta_types.drashta_types_table import TTableData
-from drashta_types.drashta_types_data import TDataArray
+from drashta_types.drashta_types_table import TTableData, TTableStructure, TDataArray
+from drashta_types.drashta_types_data import TDataSeriesHead
 import pandas
 from pydantic import BaseModel
 from typing import List
@@ -29,6 +29,27 @@ async def request_table_data(table: str, columns: list[str], size: int) -> TTabl
         response.raise_for_status()  # Ensures request was successful
 
         return response.json()  # Correct way to parse JSON from HTTP response
+
+async def request_table_column_data(table:str)->TTableStructure:
+    bridge_server_url = os.getenv("BRIDGE_SERVER")
+    if not bridge_server_url:
+        raise RuntimeError("BRIDGE_SERVER environment variable is not set")
+    async with httpx.AsyncClient() as client:
+        response:list[TTableStructure] = await client.post(
+            f"{bridge_server_url}/supabase/get-database-structure"
+        )
+    for i in response:
+        if i.table_name == table:
+            return i
+
+async def request_column_data(table:str,columns:list[str])->list[TDataSeriesHead]:
+    column_head_data:list[TDataSeriesHead] = []
+    table_data:TTableStructure = request_table_column_data(table)
+    table_columns:list[TDataSeriesHead] = table_data.table_column_head_data
+    for col in table_columns:
+        if col.column_name in columns:
+            column_head_data.append(col)
+    return column_head_data
     
 async def get_table_data(table:str,columns:list[str],size:int)->TDataArray:
     return (await request_table_data(table,columns,size))["table_data_series"]
